258fd91f41497eb9f7e5512452353f3f8e80c39c
Cristina M <mcristinamotoc@gmail.com>
2020-10-23 23:17:56 +0200

    Rename

---
+++ Ex.4/suma.js
@@ -0,0 +1,6 @@
+//Exercici 4: Crea dos variables amb dos números i fes una suma entre ells.
+
+var n1 = 34;
+var n2 = 56;
+var suma = (n1 + n2);
+console.log("La suma entre " + n1 + " i " + n2 + " es " + suma);
