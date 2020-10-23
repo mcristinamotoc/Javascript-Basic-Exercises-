a0f4a610cb4750698301da69d60c6e608714ac02
Cristina M <mcristinamotoc@gmail.com>
2020-10-23 23:20:17 +0200

    Ex6

---
+++ Ex.6/palabras.js
@@ -0,0 +1,7 @@
+//Exercici 6: Reemplaça la paraula blau per la paraula verd del següent text Tinc un cotxe de color blau. Després intenta fer-ho remplaçant les o per les u
+
+var text = "Tinc un coche blau";
+text2 = text.replace("blau", "verd");
+console.log(text2);
+text3 = text.replace("o", "u");
+console.log(text3);
