1a6ae4a023c87d6844c01d6cc6904f8213178403
Cristina M <mcristinamotoc@gmail.com>
2020-10-23 23:14:58 +0200

    Ex3

---
+++ Ejercicio3/nombreCompleto.js
@@ -0,0 +1,5 @@
+//Exercici 3:Crea una variable que contingui el teu nom i un altre que contingui el teu cognom.
+
+let nombre = "Cristina";
+let apellido = "Motoc";
+console.log(nombre + " " + apellido);
